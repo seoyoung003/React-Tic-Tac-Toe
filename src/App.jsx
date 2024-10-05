18b96bfa33c1afb151f0f1d6c56e88ca94cbd1d0
seoyoungKim <seoyoungkim003@gmail.com>
2024-10-05 22:26:04 +0900

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
 import Player from "./components/player";
-import { useState } from "react";
 
 function App() {
-  const [name, setName] = useState();
-  function handleOnclick() {
-    
-
-  }
+  
   return (
     <main>
       <div id="game-container">
         <ol id="players">
           <Player 
-            name="Player1" 
+            initialName="Player1" 
             symbol="X" 
-            Onclick = {() => handleOnclick(<input type="text"></input>)}
+            
           />
           <Player 
-            name="Player2" 
+            initialName="Player2" 
             symbol="O"
-            Onclick = {() => handleOnclick()}
+           
           />
         </ol>
       </div>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,39 @@
-export default function Player({name, symbol, ...props}) {
+import { useState } from "react";
+
+export default function Player({initialName, symbol}) {
+  const [playerName, setPlyerName] = useState(initialName);
+  const [ isEditing, setIsEditing ] = useState(false);
+
+  function handleEditingClick() {
+
+    // setIsEditing(isEditing ? false : true); 이 코드를 더 간결하게 바꿀 수 있다
+    // setIsEditing(!isEditing);
+    setIsEditing((editing) => !editing);
+
+  }
+
+  function handleChange(event) {
+   
+    setPlyerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  // let btnCaption = 'Edit';
+
+  if(isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange}/>
+    );
+    
+    // btnCaption = 'Save';
+  }
     return(
         <li>
         <span className="player">
-          <span className="player-name">{name}</span>
+          {editablePlayerName}
           <span className="player-symbol">{symbol}</span>
         </span>
-        <button {...props}>Edit</button>
+        <button onClick={ handleEditingClick }>{isEditing ? 'Save' : 'Edit'}</button>
       </li>
      
     );
